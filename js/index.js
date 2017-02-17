f44e8e8fba93839e5b26ce0b1150a3468701d228
taras <tarasovych.ua@gmail.com>
2017-02-18 00:17:58 +0200

    1

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function calc1() {
+  var gold1, dps1, res1
+  gold1 = parseInt(document.getElementById("gold1").value);
+  dps1 = parseInt(document.getElementById("dps1").value);
+  res1 = gold1*dps1; 
+  document.getElementById("res1").value = res1;
+}
